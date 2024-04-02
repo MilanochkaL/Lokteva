07b46387e043ff9c6df7f5f2312f6ea6831ccec3
Milana <Milka.lokteva@yandex.ru>
2024-04-02 13:53:29 +0300

    Добавила обработку имени

--- Lokteva/myform.py
+++ Lokteva/myform.py
@@ -10,8 +10,11 @@ def my_form():
     if not mail or not username:
         return "Please fill in all fields"
     
-    if not re.match(r"[^@]+@[^@]+\.[^@]+", mail):
+    if not re.match(r"^[^@]{1,16}@[A-Za-z]{1,7}..[A-Za-z]{2,3}$", mail):
         return "Please enter a valid email address"
+    
+    if not re.match(r"^{3,20}$", username):
+            return "Please enter a correct name (long:3-20)"
     
     current_date = datetime.datetime.now().strftime("%Y-%m-%d")
     
