8eb15432483f4482e0108e80b0b0ea9f5d1aa6ca
Milana <Milka.lokteva@yandex.ru>
2024-04-03 11:00:02 +0300

    изменила проверку email

--- Lokteva/myform.py
+++ Lokteva/myform.py
@@ -4,18 +4,24 @@ import datetime
 
 @post('/home', method='post')
 def my_form():
+    # Получаем значения из формы
     mail = request.forms.get('ADRESS')
     username = request.forms.get('USERNAME')
     
+    # Проверяем заполнены ли все поля
     if not mail or not username:
         return "Please fill in all fields"
     
-    if not re.match(r"^[^@]{1,16}@[A-Za-z]{1,7}..[A-Za-z]{2,3}$", mail):
+    # Проверяем правильность формата email
+    if not re.match(r"^[^\W^_]{1}[^@]{1,16}@[A-Za-z]{1,7}(?:.[A-Za-z]{2,3}){1,3}$", mail):
         return "Please enter a valid email address"
     
-    if not re.match(r"^{3,20}$", username):
-            return "Please enter a correct name (long:3-20)"
+    # Проверяем длину имени пользователя
+    if not re.match(r"^[\w]{3,20}$", username):
+        return "Please enter a correct name"
     
+    # Получаем текущую дату
     current_date = datetime.datetime.now().strftime("%Y-%m-%d")
     
+    # Возвращаем сообщение с данными пользователя
     return "Thanks, %s! The answer will be sent to the mail %s. Access Date: %s" % (username, mail, current_date)
