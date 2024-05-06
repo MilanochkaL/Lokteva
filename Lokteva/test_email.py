4fe116a7a3e3a766665ad3806e85df05d63fc782
Milana <Milka.lokteva@yandex.ru>
2024-05-06 18:35:23 +0300

    Добавила модульные тесты

--- Lokteva/test_email.py
+++ Lokteva/test_email.py
@@ -1,8 +1,17 @@
 import unittest
+import myform
 
 class Test_test_email(unittest.TestCase):
-    def test_A(self):
-        self.fail("Not implemented")
+    def test_invalid_emails(self):
+        list_mail_uncor = ["", "1", "m1@", "@mail", "m@m@mail.ru", "?23@mail.ru", "12123@mail..r"]
+        for email in list_mail_uncor:
+            self.assertFalse(myform.check_email_d(email))
+
+    def test_valid_emails(self):
+        list_mail_cor = ["mm@mail.ru", "m1@gmail.com", "test@example.com", "johnDoe@domain.co.uk",
+                         "jane_doe@sub.example.org", "aa@bd.cc"]
+        for email in list_mail_cor:
+            self.assertTrue(myform.check_email_d(email))
 
 if __name__ == '__main__':
     unittest.main()
